1f4cf3e80d8fadae0677f9bffc9fdcd9a92e94ae
jielab <jiehuang001@gmail.com>
2023-06-05 16:00:21 +0800

    JH update

--- scripts/analysis-101.R
+++ scripts/analysis-101.R
@@ -1,4 +1,4 @@
-setwd("C:/Users/jiehu/Desktop")
+setwd("C:/Users/jiehu/Desktop/tmp")
 pacman::p_load(data.table, lubridate, tidyverse, dplyr, ggplot2, CMplot, TwoSampleMR, MendelianRandomization, survival, survminer, mediation)
 inormal <- function(x) qnorm((rank(x, na.last = "keep") - 0.5) / sum(!is.na(x)))
 
@@ -22,7 +22,7 @@ dat <- dat0 %>% filter(ethnicity_gen==1) %>%
 		age_mn_3p = ifelse(age_mn %in% 12:14, "normal", ifelse(age_mn>14, "late", "early")),
 		age_mn_3p = factor(age_mn_3p, levels=c("normal", "early", "late"))
 	)
-varXs <- c("sp1.M", "sp1.S", "sp1.Z", "lct.rs4988235_G", "age_mn_3p")
+varXs <- c("sp1.M", "sp1.S", "sp1.Z", "lac.rs4988235_G", "age_mn_3p")
 for (varY in c("icd_copd_date", "icd_covid_date", "fod_t1dm", "fod_t2dm", "fod_dm")) {
 	dat1 <- dat %>% 
 	mutate(
@@ -36,9 +36,11 @@ for (varY in c("icd_copd_date", "icd_covid_date", "fod_t1dm", "fod_t2dm", "fod_d
 	for (varX in varXs) {
 		dat1$varX <- dat1[[varX]]
 		fit.suv <- survival::survfit(surv.obj ~ varX + sex, data=dat1)
-		survminer::ggsurvplot(fit.suv, ylim=c(0.95,1), data=dat1) 
+			png(file=paste(varX,varY,"fit.png",sep="."), w=800, h=600)
+			ggsurvplot(fit.suv, ylim=c(0.95,1), data=dat1); dev.off() 
 		fit.cox <- coxph(surv.obj ~ varX + age+sex, data=dat1) 
-		ggforest(fit.cox, data=dat1)
+			png(file=paste(varX,varY,"frt.png",sep="."), w=600, h=800)
+			ggforest(fit.cox, data=dat1); dev.off()
 	}
 }
 for (varY in grep("^bb_|^bc_", names(dat), value=T)) {
