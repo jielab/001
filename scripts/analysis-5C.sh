715746c0854d1517fe09a6a96c5104434ab3e1f3
jielab <jiehuang001@gmail.com>
2023-07-07 05:02:18 +0800

    Update analysis-5C.sh

--- scripts/analysis-5C.sh
+++ scripts/analysis-5C.sh
@@ -116,7 +116,7 @@ for exp in $dats_x; do
 	exp_ieu=NA; exp_file0=$exp_dir/$exp.gz; exp_pheno=$exp
 	exp_iv_file=$exp_dir/$exp.top.snp
 	if [ ! -e $exp_iv_file ]; then
-		exp_iv_file=$exp.top.snp; zcat $exp_file0 | awk 'NR==1 || $10<=5e-6 {b=sprintf("%.0f",$3/1e6); print $4,$2,$3,$10,b}' | sort -k 2,2n -k 5,5n -k 4,4g | awk '{if (arr[$NF] !="Y") print $1; arr[$NF] ="Y"}' > $exp.top.snp
+		exp_iv_file=$exp.top.snp; zcat $exp_file0 | awk 'NR==1 || $10<=5e-8 {b=sprintf("%.0f",$3/1e6); print $4,$2,$3,$10,b}' | sort -k 2,2n -k 5,5n -k 4,4g | awk '{if (arr[$NF] !="Y") print $1; arr[$NF] ="Y"}' > $exp.top.snp
 	fi
 	exp_iv_line=`wc -l $exp_iv_file | awk '{printf $1}'`
 	if [ "$exp_iv_line" -le 1 ]; then
@@ -153,6 +153,7 @@ for exp in $dats_x; do
 		R CMD BATCH $exp.$out.R
 	done
 done
+#可用下面的代码检查上面的程序是否全部跑完
 for exp in $dats_x; do
 	echo check $exp
 	exp_iv_file=$exp.top.snp
