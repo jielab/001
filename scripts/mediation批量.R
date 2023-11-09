63dfaf020916655ff5940aeae3bd4a0e515f520b
jielab <jiehuang001@gmail.com>
2023-11-09 10:32:42 +0800

    JH update

--- scripts/mediation批量.R
+++ scripts/mediation批量.R
@@ -1,7 +1,7 @@
 setwd("D:/")
-pacman::p_load(readxl, tidyverse, TwoSampleMR)
+pacman::p_load(readxl, tidyverse, TwoSampleMR, RMediation)
 
-analysis <- "mb"
+analysis <- "pheno"
 dir_mr <- paste0("D:/analysis/mr/", analysis) # MR分析结果所在地
 dir_M <- paste0("D:/data/gwas/", "pheno") # mediator的GWAS和top.snp所在地
 dir_Y <- paste0("D:/data/gwas/", "pheno") #
@@ -11,10 +11,10 @@ for (m in c('bb_ALB', 'bb_ALP', 'bb_ALT', 'bb_APOA', 'bb_APOB', 'bb_AST', 'bb_AS
 	dat_M0 <- read.table(paste0(dir_M, "/", m, ".gz"), header=T)
 	snp_M <- read.table(paste0(dir_M, "/", m, ".top.snps"), header=T) # dir_M 或者 dir_mr
 	dat_M4y <- merge(dat_M0, snp_M) %>% format_data(type="exposure", snp_col="SNP", effect_allele_col="EA", other_allele_col="NEA", beta_col="BETA", se_col="SE", pval_col="P")
-	for (x in rownames(xys)) {
+	for (x in grep("^x.height|^x.bmi", rownames(xys), value=T)) {
 	for (y in grep("^y.vte|^y.cad", names(xys), value=T)) { # invert=T
 		ivw_p <- xys[x,y]
-		if (ivw_p <= 5e-8) {
+		if (ivw_p <= 1e-6) {
 			dat_X <- read.table(paste0(dir_mr, "/", x, "/", x, ".txt"), header=T) %>% format_data(type ="exposure", snp_col="SNP", effect_allele_col="EA", other_allele_col="NEA", beta_col="BETA", se_col="SE", pval_col="P") 
 			dat_Y0 <- read.table(paste0(dir_Y, "/", y, ".gz"), header=T) 
 			dat_M4x <- dat_M0 %>% merge(subset(dat_X, select="SNP"))
@@ -32,8 +32,8 @@ for (m in c('bb_ALB', 'bb_ALP', 'bb_ALT', 'bb_APOA', 'bb_APOB', 'bb_AST', 'bb_AS
 			res_M2Y <- TwoSampleMR::mr(dat_MY) %>% filter(method=="Wald ratio" | method=="Inverse variance weighted") # 1st step 三角形的下坡
 				beta_M2Y <- res_M2Y %>% pull(b); se_M2Y <- res_M2Y %>% pull(se); p_M2Y <- res_M2Y %>% pull(pval)
 			beta_2step <- round(beta_X2M * beta_M2Y, 4); beta_2step
-			se_2step = round((se_X2Y^2 + se_M2Y^2)^0.5, 4)
-			p_2step <- signif(2*pnorm(abs(beta_2step/se_2step), lower.tail=F), 3); p_2step
+				CIs = RMediation::medci(beta_X2M, beta_M2Y, se_X2M, se_M2Y); se_2step = CIs$SE 
+				p_2step <- signif(2*pnorm(abs(beta_2step/se_2step), lower.tail=F), 3); p_2step
 			res_str <- paste(x, m, y, nrow(dat_XM), nrow(dat_MY), nrow(dat_XY), beta_2step, se_2step, p_2step, sep='|')
 			write.table(res_str, paste0(analysis,".out"), append=T, quote=F, row.names=F, col.names=F)
 		}
