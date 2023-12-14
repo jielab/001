ab65af13c9c15bd5d86019aaa64536ab83d22009
jielab <jiehuang001@gmail.com>
2023-12-14 15:45:13 +0800

    Update 101.复现及模拟.R

--- scripts/101.复现及模拟.R
+++ scripts/101.复现及模拟.R
@@ -43,14 +43,14 @@ print(paste(round(beta_X2Y,3),round(se_X2Y,3),p_X2Y, round(beta_X2M,3),round(se_
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 summary(lm(X ~ G, dat)); summary(lm(X ~ G1, dat)) # G强，G1弱
 summary(lm(M ~ G, dat)); summary(lm(Y ~ G, dat)) #由于样本量不够大，G和Y没有显著性
-dat$X.byG = predict.lm( lm( X ~ G, data=dat)) 
-dat$X.byG1 = predict.lm( lm( X ~ G1, data=dat)) 
-summary(lm(M ~ X, dat));  summary(lm(M ~ X.byG, dat)) # 对于一个很强的G，G基本就可代表X
-summary(lm(M ~ X, dat));  summary(lm(M ~ X.byG1, dat)) # 但对于一个弱的G1，“G1所决定的XX” 跟 “XX”本身，差别很大
-summary(lm(Y ~ X.byG, data=dat))
-	beta.G2X = summary(lm( X ~ G, data=dat))$coef[2,1]
-	beta.G2Y = summary(lm( Y ~ G, data=dat))$coef[2,1]
-	se.G2X = summary(lm( X ~ G, data=dat))$coef[2,2]
-	se.G2Y = summary(lm( Y ~ G, data=dat))$coef[2,2]
-	beta.wald = beta.G2Y / beta.G2X; beta.wald
-	se.wald = se.G2Y / beta.G2X; se.wald 
+dat$X_byG = predict.lm( lm( X ~ G, data=dat)) 
+dat$X_byG1 = predict.lm( lm( X ~ G1, data=dat)) 
+summary(lm(M ~ X, dat));  summary(lm(M ~ X_byG, dat)) # 对于一个很强的G，G基本就可代表X
+summary(lm(M ~ X, dat));  summary(lm(M ~ X_byG1, dat)) # 但对于一个弱的G1，“G1所决定的XX” 跟 “XX”本身，差别很大
+summary(lm(Y ~ X_byG, data=dat))
+	beta_G2X = summary(lm( X ~ G, data=dat))$coef[2,1]
+	beta_G2Y = summary(lm( Y ~ G, data=dat))$coef[2,1]
+	se_G2X = summary(lm( X ~ G, data=dat))$coef[2,2]
+	se_G2Y = summary(lm( Y ~ G, data=dat))$coef[2,2]
+	beta_wald = beta_G2Y / beta_G2X; beta_wald
+	se_wald = se_G2Y / beta_G2X; se_wald 
