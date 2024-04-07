83daa06bd7d79a4d42bf1d74b57aaeba24f9f40d
Jie Huang <jiehuang001@gmail.com>
2024-04-07 08:41:45 +0800

    Update phe.R

--- scripts/phe.R
+++ scripts/phe.R
@@ -1,4 +1,3 @@
-# Lifestyle和SES相关代码，可参考 https://github.com/XiangyuYe/Infection_SES/
 setwd("D:/")
 pacman::p_load(data.table, dplyr, tidyverse, crosswalkr, lubridate, naniar)
 indir = "D:/data/ukb/phe/"
@@ -120,7 +119,7 @@ for (i in 1:nrow(vip)) {
 }
 fod <- subset(bd, select=grep("eid|fod_", names(bd), value=T)) %>% rename(eid=f.eid)
 saveRDS(fod, file="D:/data/ukb/Rdata/ukb.fod.rds")
-???? 新方法？
+# 下为南京医科大学版本，未经测试！
 for (type in c("", "01_Respiratory","02_Digestive", "03_Blood_Sexually", "04_Bacterial")) {
   data_path <- paste0("~/Infection_SES/", type,"/")
   infect10_code <- fread2(paste0(data_path,"ICD10_infect.txt"))[,"Value"]
@@ -145,62 +144,81 @@ for (type in c("", "01_Respiratory","02_Digestive", "03_Blood_Sexually", "04_Bac
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # lifestyle 之 “管住嘴、 迈开腿、 躺下睡”
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#健康植物性饮食指数（PMID: 36976560）
+# 南京医科大学版本（https://github.com/XiangyuYe/Infection_SES）
 dat <- phe0 %>% 
-	mutate(
-		num_pa = (days_pa_mod > 5) & (days_pa_vig > 1),
-		yes_pa_mod = ifelse(dura_pa_mod > 150, T, F),
-		yes_pa_vig = ifelse(dura_pa_vig > 75, T, F),
-		pa_score =0,
-		mat_pa = cbind(num_pa, yes_pa_mod, yes_pa_vig)
-		pa_score[rowSums(mat_pa, na.rm=T) > 0] <- 1,
-		pa_score[rowSums(is.na(mat_pa)) == 3] <- NA
-	)
-	fruit_score[rowSums(cbind(fruit_fresh, fruit_dried/5), na.rm = T) >= 4] <- T
-	veg_score[rowSums(cbind(veg_cooked/3, veg_salad/3), na.rm=T) >= 4] <- T # vegetable: per serving = cooked/raw vegetables - 3 heaped tablespoons
-	fish_score = (fish_oily >= 3) | (fish >= 3)
-	fish_score[fish_oily ==2 & fish == 2] <- T
-	pmeat_score = meat_processed <= 2
-	npmeat_mat = cbind(beef, lamb, pork)
-	npmeat_score = rowSums(npmeat_mat, na.rm = T) <= 3
-	npmeat_score[npmeat_mat[,1] >=3 | npmeat_mat[,2] >=3 | npmeat_mat[,3] >=3] <- F
-	grains_mat = matrix(0, nrow(habit_baseline_df), 2)
-	bread_idx = which(bread_type == 3); grains_mat[bread_idx, 1] <- bread[bread_idx]
-	cereal_idx = which(cereal_type %in% c(1, 2, 3))
-	grains_mat[cereal_idx, 2] = cereal[cereal_idx]
-	grains_score = rowSums(grains_mat, na.rm = T) >= 3
-	diet_mat = data.frame(fruit_score, veg_score, fish_score, pmeat_score, npmeat_score, grains_score)
-	diet_score = rowSums(diet_mat) >= 4
-	diet_score[rowSums(diet_mat,na.rm = T) >= 4] <- T
-	diet_score[rowSums(is.na(diet_mat)) + rowSums(diet_mat,na.rm = T) < 4] <- F
-chrono_score = ifelse(sleep_chronotype < 3, 1, 0) # Ref PMID:31848595
-	duration_score = ifelse(sleep_duration <= 8 & sleep_duration >= 7, 1, 0)
-	insomnia_score = ifelse(sleep_insomnia == 1, 1, 0)
-	snoring_score = ifelse(sleep_snoring == 2, 1, 0)
-	narcolepsy_score = ifelse(sleep_narcolepsy < 2, 1, 0)
-	sleep_mat = data.frame(chrono_score, duration_score, insomnia_score, snoring_score, narcolepsy_score)
-	sleep_score = rowSums(sleep_mat) >= 4
-	sleep_score[rowSums(sleep_mat,na.rm = T) >= 4] <- T
-	sleep_score[rowSums(is.na(sleep_mat)) + rowSums(sleep_mat,na.rm = T) < 4] <- F
-lf_df = data.frame(Nosmoke = nosmoke_cnd, Activity = activity_cnd, Diet = diet_cnd, Noalcohol = noalcohol_cnd, Sleep = sleep_cnd, Nocannabis = cannabis_cnd)
-	lifescore1 = rep(NA, nrow(lf_df))
-	lifescore1[rowSums(lf_df, na.rm = T) >= 4] <- 3
+  mutate(
+    num_pa = (days_pa_mod > 5) & (days_pa_vig > 1),
+    yes_pa_mod = ifelse(dura_pa_mod > 150, T, F),
+    yes_pa_vig = ifelse(dura_pa_vig > 75, T, F),
+    mat_pa = cbind(num_pa, yes_pa_mod, yes_pa_vig),
+    pa_score = rep(0, nrow(mat_pa))
+  ) %>% 
+  mutate(
+    pa_score = ifelse(rowSums(mat_pa, na.rm = TRUE) > 0, 1, pa_score),
+    pa_score = ifelse(rowSums(is.na(mat_pa)) == 3, NA, pa_score),
+    cannabis_score = ifelse(cannabis == 0, 1, ifelse(cannabis >0, 0, NA)),
+    noalcohol_stat = recode(alcohol_status, "never" = 0, "previous" = 1, "current" = 2),
+    noalcohol_score = ifelse(noalcohol_stat == 0, 1, ifelse(noalcohol_stat > 0, 0, NA)),
+    smoke_stat = recode(smoke_status, "never" = 0, "previous" = 1, "current" = 2),
+    nosmoke_score = ifelse(smoke_stat == 0, 1, ifelse(smoke_stat >0 , 0, NA)),
+    nosmoke_year = age_visit - age_smoke_quit,
+    nosmoke_score = ifelse(nosmoke_year > 30, 1, nosmoke_score)
+  )%>%
+  mutate(
+    fruit_score = rowSums(cbind(fruit_fresh, fruit_dried/5), na.rm = TRUE) >= 4,
+    fruit_score = ifelse(is.na(fruit_fresh) & is.na(fruit_dried), NA, fruit_score),
+    veg_score = rowSums(cbind(veg_cooked/3, veg_salad/3), na.rm = TRUE) >= 4,
+    veg_score = ifelse(is.na(veg_cooked) & is.na(veg_salad) , NA, veg_score),
+    fish_score = (fish_oily >= 3 | fish >= 3) | (fish_oily == 2 & fish == 2),
+    pmeat_score = meat_processed <= 2,
+    npmeat_score = rowSums(cbind(beef, lamb, pork)) <= 3 & !(beef >= 3 | lamb >= 3 | pork >= 3),  # label1 =0.5, then 1 + 2 OR 1 * 3
+    npmeat_score = ifelse(is.na(beef) & is.na(lamb) & is.na(pork), NA, npmeat_score),
+    bread_grain = ifelse(bread_type==3, bread, 0),
+    cereal_grain = ifelse(cereal_type %in% c(1, 2, 3), cereal, 0),
+    grains_score = rowSums(cbind(bread_grain, cereal_grain), na.rm = T) >= 3,
+    grains_score = ifelse(is.na(bread_grain) & is.na(cereal_grain) , NA, grains_score)
+  ) %>%
+  mutate(
+    diet_mat = data.frame(fruit_score, veg_score, fish_score, pmeat_score, npmeat_score, grains_score),
+    diet_score = rowSums(diet_mat) >= 4,
+    diet_score = ifelse(rowSums(diet_mat, na.rm = TRUE) >= 4, TRUE, ifelse(rowSums(is.na(diet_mat)) + rowSums(diet_mat, na.rm = TRUE) < 4, FALSE, NA))
+  )%>% mutate(
+    chrono_score = ifelse(sleep_chronotype < 3, 1, 0), # Ref PMID:31848595
+    duration_score = ifelse(sleep_duration <= 8 & sleep_duration >= 7, 1, 0), # Sleep duration 7-8h
+    insomnia_score = ifelse(sleep_insomnia == 1, 1, 0), # 1: never or rarely insomnia
+    snoring_score = ifelse(sleep_snoring == 2, 1, 0), # 2: No complaints of snoring
+    narcolepsy_score = ifelse(sleep_narcolepsy < 2, 1, 0) # no frequently narcolepsy 0	Never/rarely 1	Sometimes
+  ) %>% mutate(
+    sleep_mat = data.frame(chrono_score, duration_score, insomnia_score, snoring_score, narcolepsy_score),
+    sleep_score = rowSums(sleep_mat) >= 4,
+    sleep_score = ifelse(rowSums(sleep_mat, na.rm = TRUE) >= 4, TRUE, sleep_score),
+    sleep_score = ifelse(rowSums(is.na(sleep_mat)) + rowSums(sleep_mat, na.rm = TRUE) < 4, FALSE, sleep_score)
+  )
+lf <- dat[, c("eid","smoke_score","pa_score" ,"diet_score", "alcohol_score","sleep_score","cannabis_score")]
+	lf_df <- lf*1
+lifescore1 <- rep(NA, nrow(lf_df))
+	lifescore1[rowSums(lf_df, na.rm = T) >= 4] <- 3 
 	lifescore1[rowSums(lf_df[,c(1:5)], na.rm = T) == 3] <- 3
 	lifescore1[rowSums(lf_df, na.rm = T) >= 2 & (rowSums(lf_df, na.rm = T) + rowSums(is.na(lf_df))) < 4 ] <- 2 
-	lifescore1[(rowSums(lf_df, na.rm = T) + rowSums(is.na(lf_df))) < 2 | rowSums(lf_df, na.rm = T) == 0] <- 1 
-	lifescore2 = rep(2, nrow(lf_df)); lifescore2[rowSums(lf_df, na.rm = T) >= 4] <- 3
-	lifescore2[rowSums(lf_df[,c(1:5)], na.rm = T) >= 3] <- 3
+	lifescore1[(rowSums(lf_df, na.rm = T) + rowSums(is.na(lf_df))) < 2 |  rowSums(lf_df, na.rm = T) == 0] <- 1 
+lifescore2 <- rep(2, nrow(lf_df))
+	lifescore2[rowSums(lf_df, na.rm = T) >= 4] <- 3 
+	lifescore2[rowSums(lf_df[,c(1:5)], na.rm = T) >= 3] <- 3 
 	lifescore2[rowSums(lf_df == 0, na.rm = T) >= 4] <- 1
-	lifescore3 = rep(2, nrow(lf_df)); lifescore3[rowSums(lf_df, na.rm = T) >= 4] <- 3
-	lifescore3[rowSums(lf_df[,c(1:5)], na.rm = T) >= 3] <- 3
+lifescore3 <- rep(2, nrow(lf_df))
+	lifescore3[rowSums(lf_df, na.rm = T) >= 4] <- 3 
+	lifescore3[rowSums(lf_df[,c(1:5)], na.rm = T) >= 3] <- 3 
 	lifescore3[rowSums(lf_df == 0, na.rm = T) >= 5] <- 1
-	life_factor_df = data.frame(eid "_score", lifescore1, lifescore2, lifescore2)
+	life_factor_df = data.frame(lf_df, lifescore1, lifescore2, lifescore3)
+saveRDS(life_factor_df, file = "D:/data/ukb/Rdata/ukb.life.rds")
 
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # 空气和噪音污染
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~	      
 air_no2, air_nox, 
-noise = rowMeans(cbind(noise_day, noise_evening+5,noise_night+10), na.rm = T),
+noise = rowMeans(cbind(noise_day, noise_evening+5, noise_night+10), na.rm = T),
 traffic, dist2road = 1/dist2road, log_traffic = log(traffic), logi_dist2road = -log(dist2road)
 
 
