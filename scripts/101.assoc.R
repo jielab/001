4ecf6dadefbb82ef81e6ee97c979dc0a48d84ce8
Jie Huang <jiehuang001@gmail.com>
2024-05-06 11:22:39 +0800

    Update 101.assoc.R

--- scripts/101.assoc.R
+++ scripts/101.assoc.R
@@ -10,7 +10,7 @@ dat0 <- readRDS(file="D:/data/ukb/Rdata/all.Rdata") %>% mutate(birth_month=facto
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # X-Y 或 X-Y-Z交互作用 批量分析
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-dat <- dat0 %>% drop_na(age, sex) %>% filter(ethnic_cat=="White", sex==1)
+dat <- dat0 %>% drop_na(age, sex) %>% filter(ethnic_cat=="White")
 Ys <- grep("^icdDate_", names(dat), value=TRUE)
 Xs <- grep("^age_sex|age_m|^edu_score|^birth_weight|birth_month|^height$|^chunk|^leg|^hippo_|^fev1fvc|^stiffness|score_sum$", names(dat), value=TRUE) 
 Zs <- grep("^o$|^se$", names(dat), value=TRUE) # |^rh|shbg|^apoe$|\\.rs
@@ -73,7 +73,7 @@ dat1 <- dat1 %>%
 		Z_qt = cut(Z, breaks=quantile(Z, probs=seq(0,1,0.2), na.rm=T), include.lowest=T, labels=paste0("q",1:5)),
 	#	X_qt = factor(ifelse(X_qt=="q1", "loss", ifelse(X_qt=="q5", "gain", "same")), levels=c("loss", "same", "gain")),
 		Z_qt = factor(ifelse(hardcall(vte.F5.rs6025_C)!=2, "F5", ifelse(vte.F2.rs1799963_G !=2, "F2", ifelse(Z_qt=="q1", "low", ifelse(Z_qt=="q5", "high", "middle")))), levels=c("low", "middle", "high", "F2", "F5")),
-		X_Z = factor(paste(X, Z_qt, sep="|"), levels=c("low|brisk","low|steady","low|slow", "middle|brisk","middle|steady","middle|slow", "high|brisk","high|steady","high|slow",  "F2|brisk","F2|steady","F2|slow", "F5|brisk","F5|steady", "F5|slow")),
+		X_Z = factor(paste(Z_qt, X, sep="|"), levels=c("low|brisk","low|steady","low|slow", "middle|brisk","middle|steady","middle|slow", "high|brisk","high|steady","high|slow",  "F2|brisk","F2|steady","F2|slow", "F5|brisk","F5|steady", "F5|slow")),
 		Y_yes = ifelse(is.na(Y_date), 0, 1),
 		follow_end_day = fifelse(!is.na(Y_date), Y_date, fifelse(!is.na(date_lost), date_lost, fifelse(!is.na(date_death), date_death, as.Date("2021-12-31")))),
 		follow_years = (as.numeric(follow_end_day) - as.numeric(date_attend)) / 365.25,
@@ -84,7 +84,7 @@ surv.obj <- Surv(time=dat1$follow_years, event=dat1$Y_yes)
 km.obj <- survfit(surv.obj ~ Z_qt, data=dat1)
 	plot(km.obj, ylim=c(0.5,1)); plot(km.obj, fun=function(x) 1-x)
 	ggsurvplot(km.obj, ylim=c(0,0.1), fun="event", break.time.by=2, risk.table=FALSE, censor=FALSE, pval=TRUE) #palette=c("") 
-fit.cox <- coxph(surv.obj ~ X_Z +walk_time+walk_freq +age+sex+bmi+PC1+PC2+ smoke_status+alcohol_status, data=dat1); print(coef(summary(fit.cox)))
+fit.cox <- coxph(surv.obj ~ X_Z +walk_time+walk_freq +age+bmi+PC1+PC2+ smoke_status+alcohol_status, data=dat1); print(coef(summary(fit.cox)))
 	survminer::ggforest(fit.cox, main="", cpositions=c(0, 0.1, 0.3), fontsize=1.2, data=dat1) # 不能显示interaction值
 	fit.cox %>% gtsummary::tbl_regression(exponentiate=TRUE) %>% plot()
 		fit.glm <- glm(outcome_yes ~ gen, data=dat, family="binomial")
