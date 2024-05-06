ac763a5ab7141f91963d166e726ffb7a26f14694
Jie Huang <jiehuang001@gmail.com>
2024-05-06 11:07:31 +0800

    Update 101.assoc.R

--- scripts/101.assoc.R
+++ scripts/101.assoc.R
@@ -10,8 +10,7 @@ dat0 <- readRDS(file="D:/data/ukb/Rdata/all.Rdata") %>% mutate(birth_month=facto
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # X-Y 或 X-Y-Z交互作用 批量分析
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-dat <- dat0 %>% drop_na(age, sex) %>% filter(ethnic_cat=="White") %>% 
-	mutate(across(grep("walk", names(dat0), value=T), ~factor(.x)))
+dat <- dat0 %>% drop_na(age, sex) %>% filter(ethnic_cat!="White")
 Ys <- grep("^icdDate_", names(dat), value=TRUE)
 Xs <- grep("^age_sex|age_m|^edu_score|^birth_weight|birth_month|^height$|^chunk|^leg|^hippo_|^fev1fvc|^stiffness|score_sum$", names(dat), value=TRUE) 
 Zs <- grep("^o$|^se$", names(dat), value=TRUE) # |^rh|shbg|^apoe$|\\.rs
@@ -61,28 +60,44 @@ library(ggcorrplot); plot_bp <- ggcorrplot::ggcorrplot(dat_b, lab=TRUE, p.mat=da
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # 针对某一*显著*结果的精细分析
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-dat1 <- dat %>% dplyr::select(height, chunk, leg, chunk_ratio, icdDate_cad, icdDate_vte, icdDate_lung_cancer, date_attend, date_lost, date_death, age,sex,bmi,whr,smoke_status,alcohol_status,PC1,PC2)
 dat1 <- dat %>% 
-	rename(X=walk_pace, Y_date=icdDate_vte, Z=vte.nf.score_sum) %>% 
+	mutate(
+		walk_pace=4 - walk_pace, 
+		across(grep("walk", names(dat0), value=T), ~factor(.x)),
+		walk_pace=factor(walk_pace, labels=c("brisk","steady", "slow"))
+	) %>%
+	rename(X=walk_pace, Y_date=icdDate_vte, Z=vte.nf.score_sum) %>% drop_na(X, Z) 
+dat1 <- dat1 %>% 
 	mutate(
 	#	X_qt = cut(X, breaks=quantile(X, probs=seq(0,1,0.2), na.rm=T), include.lowest=T, labels=paste0("q",1:5)),
 		Z_qt = cut(Z, breaks=quantile(Z, probs=seq(0,1,0.2), na.rm=T), include.lowest=T, labels=paste0("q",1:5)),
 	#	X_qt = factor(ifelse(X_qt=="q1", "loss", ifelse(X_qt=="q5", "gain", "same")), levels=c("loss", "same", "gain")),
 		Z_qt = factor(ifelse(hardcall(vte.F5.rs6025_C)!=2, "F5", ifelse(vte.F2.rs1799963_G !=2, "F2", ifelse(Z_qt=="q1", "low", ifelse(Z_qt=="q5", "high", "middle")))), levels=c("low", "middle", "high", "F2", "F5")),
+		X_Z = factor(paste(X, Z_qt, sep="|"), levels=c("brisk|low","steady|low","slow|low", "brisk|middle","steady|middle","slow|middle", "brisk|F2","steady|F2","slow|F2", "brisk|high","steady|high","slow|high", "brisk|F5","steady|F5", "slow|F5")),
 		Y_yes = ifelse(is.na(Y_date), 0, 1),
 		follow_end_day = fifelse(!is.na(Y_date), Y_date, fifelse(!is.na(date_lost), date_lost, fifelse(!is.na(date_death), date_death, as.Date("2021-12-31")))),
 		follow_years = (as.numeric(follow_end_day) - as.numeric(date_attend)) / 365.25,
 	) %>% filter( follow_years >0 )			
-table(dat1$Y_yes, useNA="always")
-	aggregate(Y_yes ~ X_qt, dat1, FUN=function(x) { paste( length(x), sum(x), round(sum(x)/length(x),3)) } )
-	aggregate(Y_yes ~ Z_qt, dat1, FUN=function(x) { paste( length(x), sum(x), round(sum(x)/length(x),3)) } )
 coef(summary(glm(Y_yes ~ X + age+sex+smoke_status+alcohol_status +PC1+PC2, data=dat1)))
+	aggregate(Y_yes ~ X_qt, dat1, FUN=function(x) { paste( length(x), sum(x), round(sum(x)/length(x),3)) } )
 surv.obj <- Surv(time=dat1$follow_years, event=dat1$Y_yes)
 km.obj <- survfit(surv.obj ~ Z_qt, data=dat1)
 	plot(km.obj, ylim=c(0.5,1)); plot(km.obj, fun=function(x) 1-x)
-	ggsurvplot(km.obj, ylim=c(0,0.1), break.time.by=2, ggtheme=theme_classic2(), risk.table=FALSE, censor=F, fun="event", text.size=8) #palette=c("") 
-fit.cox <- coxph(surv.obj ~ X+Z1+Z2 +X*Z1_qt +X*Z2_qt + vte.nf.score_sum+walk_time+walk_freq +age+sex+bmi+PC1+PC2+ smoke_status+alcohol_status, data=dat1); print(coef(summary(fit.cox)))
-	survminer::ggforest(fit.cox, main="", fontsize=1.2, data=dat1) # 不能显示interaction值
+	ggsurvplot(km.obj, ylim=c(0,0.1), fun="event", break.time.by=2, risk.table=FALSE, censor=FALSE, pval=TRUE) #palette=c("") 
+fit.cox <- coxph(surv.obj ~ X_Z +walk_time+walk_freq +age+sex+bmi+PC1+PC2+ smoke_status+alcohol_status, data=dat1); print(coef(summary(fit.cox)))
+	survminer::ggforest(fit.cox, main="", cpositions=c(0, 0.1, 0.3), fontsize=1.2, data=dat1) # 不能显示interaction值
 	fit.cox %>% gtsummary::tbl_regression(exponentiate=TRUE) %>% plot()
 		fit.glm <- glm(outcome_yes ~ gen, data=dat, family="binomial")
 	survdiff(surv.obj ~ gen_3p, data=dat) # log-rank test
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 高阶分析和画图
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~	
+pacman::p_load(vivid, randomForest, MASS)
+set.seed(12345)
+dat2 <- dat1 %>% drop_na(X, Z, walk_time,walk_freq, age, sex, bmi,PC1,PC2,smoke_status,alcohol_status)
+rf_model <- randomForest(Y_yes ~ X+Z+ walk_time+walk_freq +age+sex+bmi+PC1+PC2+ smoke_status+alcohol_status, data=dat2)
+VIVI_rf <- vivi(fit = rf_model, response = "Y_yes", data=dat2)
+viviHeatmap(mat = VIVI_rf)
+viviNetwork(mat = VIVI_rf)
