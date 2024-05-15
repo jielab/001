f33e3474129dfdc6138bf72b91f81a6cedccc421
Jie Huang <jiehuang001@gmail.com>
2024-05-15 19:20:21 +0800

    Update 101.assoc.R

--- scripts/101.assoc.R
+++ scripts/101.assoc.R
@@ -1,25 +1,26 @@
 setwd("D:/")
-pacman::p_load(data.table, readxl, lubridate, tidyverse, dplyr, survival, survminer, gtsummary, reshape2, psych)
+pacman::p_load(data.table, readxl, lubridate, dplyr, tidyr, survival, survminer, gtsummary, reshape2, psych)
 inormal <- function(x) qnorm((rank(x, na.last="keep") - 0.5) / sum(!is.na(x)))
 std <- function(x) (x - mean(x,na.rm=T)) / sd(x,na.rm=T)
 hardcall <- function(x) ifelse(x<0.5, 0, ifelse(x<1.5, 1, 2))
 
-dat0 <- readRDS(file="D:/data/ukb/Rdata/all.Rdata") %>% mutate(birth_month=factor(birth_month))
+dat0 <- readRDS(file="D:/data/ukb/Rdata/all.Rdata")
 
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # X-Y 或 X-Y-Z交互作用 批量分析
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 dat <- dat0 %>% mutate(
+		walk_pace=4 - walk_pace, across(grep("walk", names(dat0), value=T), ~factor(.x)),
+		walk_pace=factor(walk_pace, labels=c("brisk","steady", "slow")),
 		icdDate_vte2=as.Date(ifelse(!is.na(icdDate_vte), as.character(icdDate_vte), ifelse(!is.na(srdYear_vte), (paste0(srdYear_vte,"-07-01")), ifelse(!is.na(srdAge_vte) & srdAge_vte >0, paste0(birth_year+srdAge_vte,"-07-01"), NA))))
 	) %>% filter(ethnic_cat=="White") 
 hist(dat$icdDate_vte2, breaks="year")
 Ys <- grep("^icdDate_", names(dat), value=TRUE)
-Xs <- grep("^age_sex|age_m|^edu_score|^birth_weight|birth_month|^height$|^chunk|^leg|^hippo_|^fev1fvc|^stiffness|score_sum$", names(dat), value=TRUE)
+Xs <- grep("^age_sex|age_m|^edu_score|^birth_weight|^height$|^chunk|^leg|^hippo_|^fev1fvc|^stiffness|score_sum$", names(dat), value=TRUE)
 Ms <- grep("^bmi$|bb_|bc_", names(dat), value=TRUE)
 Zs <- grep("^o$|^se$", names(dat), value=TRUE) # |^rh|shbg|^apoe$|\\.rs
-outfile="101.assoc.tsv"; file.create(outfile); 
-#sink("101.assoc.log")
+# outfile="101.assoc.tsv"; file.create(outfile); 
 for (Y in Ys) { # 🙍‍
 	writeLines(paste('\n\n--> Run:', Y))
 	dat1 <- dat %>%
@@ -33,20 +34,17 @@ for (Y in Ys) { # 🙍‍
 	for (X in Xs) { # 🍷
 		if (X==Y) next
 		print(paste("RUN", X, Y))
-		dat1$X=inormal(dat1[[X]])
+		dat1$X=dat1[[X]]
 		cox.fit <- coxph(surv.obj ~ X +age+sex +PC1+PC2+weight+smoke_status+alcohol_status, data=dat1)
 		print(res.cox <- coef(summary(cox.fit)))
 		b=round(res.cox[1,1],4); se=round(res.cox[1,3],4); p=signif(res.cox[1,5],2)
-		write.table(paste(Y, X, b, se, p), file=outfile, sep='\t', row.names=FALSE, col.names=FALSE, append=TRUE, quote=FALSE)
-		for (M in Ms) { # 🐎 
-			fit.X2M <- lm(M ~ X, data=dat); summary(fit.X2M)
-			coef.X2M <- coef(summary(fit.X2M)); coef.X2M
-			beta.X2M <- coef.X2M[2,1]; se.X2M <- coef.X2M[2,2]; p.X2M <- signif(coef.X2M[2,4],2)
-			fit.M2Y <- lm(Y ~ M+X, data=dat); summary(fit.M2Y) # ！这儿必须捎带上X！ X的BETA=0.01667，是ADE，不再显著，表明 complete mediation。
-			coef.M2Y <- coef(summary(fit.M2Y)); coef.M2Y 
-			beta.M2Y <- coef.M2Y[2,1]; se.M2Y <- coef.M2Y[2,2]; signif(coef.M2Y[2,4],2)
-			beta.X2Y.adjM <- coef.M2Y[3,1]; se.X2Y.adjM <- coef.M2Y[3,2]; signif(coef.M2Y[3,4],2)
-			res <- mediation::mediate(fit.X2M, fit.M2Y, treat="X", mediator="M", boot=T)
+		# write.table(paste(Y, X, b, se, p), file=outfile, sep='\t', row.names=FALSE, col.names=FALSE, append=TRUE, quote=FALSE)
+		for (M in Ms) { # 🐎
+			print(paste("RUN", X, Y, M))
+			dat1$M=inormal(dat1[[M]])
+			fit.X2M <- lm(M ~ X, data=dat1)
+			fit.M2Y <- survreg(Surv(follow_years, Y_yes) ~ M + X, data=dat1) # 🐕 这儿用surveg
+			res <- mediation::mediate(fit.X2M, fit.M2Y, treat="X", mediator="M")
 			print(SUM <- summary(res))
 			print(c(round(SUM$tau.coef,3), round(SUM$z.avg,3), round(SUM$n.avg,3))) # Total, ADE, prop
 			print(c(round(SUM$d.avg,3), round(SUM$d.avg.ci,3), signif(SUM$d.avg.p,2))) # ACME
@@ -78,23 +76,19 @@ library(ggcorrplot); plot_bp <- ggcorrplot::ggcorrplot(dat_b, lab=TRUE, p.mat=da
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # 针对某一*显著*结果的精细分析
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-dat1 <- dat %>% mutate(
-		walk_pace=4 - walk_pace, 
-		across(grep("walk", names(dat0), value=T), ~factor(.x)),
-		walk_pace=factor(walk_pace, labels=c("brisk","steady", "slow"))
-	) %>% rename(X=walk_pace, Y_date=icdDate_vte2, F5=vte.F5.rs6025_C, F2=vte.F2.rs1799963_G) %>% 
+dat1 <- dat %>% rename(X=walk_pace, Y_date=icdDate_vte2, F5=vte.F5.rs6025_C, F2=vte.F2.rs1799963_G) %>% 
 	drop_na(X, F2, F5) 
 dat1 <- dat1 %>% mutate(
 	#	X_qt = cut(X, breaks=quantile(X, probs=seq(0,1,0.2), na.rm=T), include.lowest=T, labels=paste0("q",1:5)),
 	#	X_qt = factor(ifelse(X_qt=="q1", "low", ifelse(X_qt=="q5", "high", "middle")), levels=c("low", "middle", "high")),
 	#	Z = factor(ifelse(hardcall(F5)!=2, "F5", ifelse(F2 !=2, "F2", ifelse(Z_qt=="q1", "low", ifelse(Z_qt=="q5", "high", "middle")))), levels=c("low", "middle", "high", "F2", "F5")),
 		Z = factor(ifelse(hardcall(F5)!=2, "F5", ifelse(hardcall(F2) !=2, "F2", "none")), levels=c("none","F2","F5")),
-		Z_X = factor(paste(Z, X, sep="|"), levels=paste(rep(levels(Z),each=3), rep(levels(X),3), sep="|")),
+		X_Z = factor(paste(Z, X, sep="|"), levels=paste(rep(levels(Z),each=3), rep(levels(X),3), sep="|")),
 		Y_yes = ifelse(is.na(Y_date), 0, 1),
 		follow_end_day = fifelse(!is.na(Y_date), Y_date, fifelse(!is.na(date_lost), date_lost, fifelse(!is.na(date_death), date_death, as.Date("2021-12-31")))),
 		follow_years = (as.numeric(follow_end_day) - as.numeric(date_attend)) / 365.25,
 	) %>% filter( follow_years >0)
-	# dat2 <- dat1 %>% dplyr::select(eid, follow_years, Y_yes, X, Z, Z_X, walk_time,walk_freq,age,sex,bmi,PC1,PC2,smoke_status,alcohol_status) 
+	# dat2 <- dat1 %>% dplyr::select(eid, follow_years, Y_yes, X, Z, X_Z, walk_time,walk_freq,age,sex,bmi,PC1,PC2,smoke_status,alcohol_status) 
 prop.table(table(dat1$X, dat1$Y_yes),1); prop.table(table(dat1$X_Z, dat1$Y_yes),1)	
 	aggregate(Y_yes ~ X, dat1, FUN=function(x) { paste( length(x), sum(x), round(sum(x)/length(x),3)) } )
 	coef(summary(glm(Y_yes ~ X+Z+ age+sex+smoke_status+alcohol_status +PC1+PC2, family="binomial", data=dat1)))
@@ -102,7 +96,7 @@ surv.obj <- Surv(time=dat1$follow_years, event=dat1$Y_yes)
 km.obj <- survfit(surv.obj ~ Z, data=dat1) # KM 是不能带协变量的，Z会被作为分层变量
 	survdiff(surv.obj ~ Z, data=dat1) # log-rank test
 	plot(km.obj, fun=function(x) 1-x)
-	ggsurvplot(km.obj, ylim=c(0,0.08), fun="event") # linetype=rep(1:3,3), palette=rep(c("green","orange","red"),3))
+	survminer::ggsurvplot(km.obj, ylim=c(0,0.08), fun="event", pval=TRUE, risk.table=TRUE, risk.table.col="Z", ncensor.plot = TRUE, ggtheme = theme_bw(), palette=c("green","gray","orange"))
 cox.fit <- coxph(surv.obj ~ X+Z+X*Z +walk_time+walk_freq +age+sex+bmi+PC1+PC2+ smoke_status+alcohol_status, data=dat1); print(coef(summary(cox.fit)))
 	survminer::ggforest(cox.fit, main="", cpositions=c(0, 0.1, 0.3), fontsize=1.2, data=dat1) # 不能显示interaction值
 	cox.fit %>% gtsummary::tbl_regression(exponentiate=TRUE) %>% plot()
