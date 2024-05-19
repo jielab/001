5f6948247839b6fef2a6849aa50d3149cd60ad3f
Jie Huang <jiehuang001@gmail.com>
2024-05-19 11:48:29 +0800

    Update 001.prs.R

--- scripts/001.prs.R
+++ scripts/001.prs.R
@@ -1,81 +1,132 @@
-pacman::p_load(dplyr, tidyverse, ggplot2, cluster, factoextra, pROC)
+setwd("D:/")
+pacman::p_load(dplyr, tidyr, ggplot2, pROC)
 std <- function(x) (x - mean(x,na.rm=T)) / sd(x,na.rm=T)
 inormal <- function(x) qnorm((rank(x, na.last = "keep") - 0.5) / sum(!is.na(x)))
-setwd("D:/")
 
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # PRS-GRID
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 set.seed(12345)
-trait='height'; binary='N' # trait='t2dm'; binary='Y'
+trait='t2dm'; model='glm' # lm 或 glm 或 coxph
 races=c("EUR", "AFR", "EAS", "SAS") 
+pcs=40 # 40个 PC
+folds=10 # 10-fold cross validation
+bootstrap=100
+
 dat0 <- readRDS("D:/data/ukb/Rdata/all.Rdata") 
 dat <- dat0 %>% subset(select=grepl("^eid|^age|^sex$|^ethnic|^PC|^umap|t2dm|height", names(dat0))) %>% 
-	filter(!is.na(PC1), !is.na(umap1), ethnic_cat %in% c("White", "Black", "Chinese", "Asian")) %>%
+	drop_na(PC1, umap1) %>% filter(ethnic_cat %in% c("White", "Black", "Chinese", "Asian")) %>%
 	mutate(race=ifelse((PC1> -25 & PC1<25 & PC2> -25 & PC2<20), "EUR",ifelse((PC1>25 & PC1<125 & PC2> -160 & PC2< -60), "SAS", ifelse((PC1>130 & PC1<180 & PC2> -290 & PC2< -250), "EAS", ifelse((PC1>250 & PC1<425 & PC2>25 & PC2<90), "AFR", NA)))))
-if (binary=="N") dat$trait=dat[[trait]]
-if (binary=="Y") dat$trait=ifelse(is.na(dat[[paste0('icdDate_',trait)]]), 0, 1)
-for (r in races){
-	dat[[paste0(r, '.prs')]] = std(dat[[paste0(trait, '.', r, '.score_sum')]]) # 可尝试去掉std()
-}
 # ggplot(dat, aes(PC1, PC2, color=ethnic_cat)) + geom_point(size=1) + scale_color_manual(labels = c("EUR", "SAS", "AFR", "EAS"), values = c("blue", "purple", "black", "red")) + theme_bw() + guides(color=guide_legend("race")) + 
 	# geom_rect(aes(xmin=-25, xmax=25, ymin=-25, ymax=20), linetype=5, fill="transparent", color="blue", size=1) +geom_rect(aes(xmin=25, xmax=125, ymin=-160, ymax=-60), linetype=5, fill="transparent", color="purple", size=1) +
 	# geom_rect(aes(xmin=130, xmax=180, ymin=-290, ymax=-250), linetype=5, fill="transparent", color="red", size=1) +geom_rect(aes(xmin=250, xmax=425, ymin=25, ymax=90), linetype=5, fill="transparent", color="black", size=1)
 	# km = kmeans(subset(dat, select=c("PC1","PC2")), centers=4, nstart=50); fviz_cluster(km, data=dat)	
-dat$PC1 <- residuals(lm(PC1 ~ AFR.prs + EAS.prs + EUR.prs + SAS.prs +age+sex, na.action=na.exclude, data=dat ))
-dat$PC2 <- residuals(lm(PC2 ~ AFR.prs + EAS.prs + EUR.prs + SAS.prs +age+sex, na.action=na.exclude, data=dat ))
-dat <- dat %>% drop_na(race, trait, EUR.prs) %>% group_by(race) %>% slice_sample(n=1000) 
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 处理PC & 计算d
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 for (r in races){
-	dat_sub <- subset(dat, race==r); print(paste(r,nrow(dat_sub)))
-	dat_sub$prs.sameRace <- std(dat_sub[[paste0(r,".prs")]])
-	assign(paste0("PC1.center.",r), median(dat_sub$PC1))
-	assign(paste0("PC2.center.",r), median(dat_sub$PC2))
-	ii <- sort(sample(1:nrow(dat_sub), round(nrow(dat_sub)*0.5)))
-	dat_sub.train <- dat_sub[ii,]
-	dat_sub.valid <- dat_sub[-ii,]
-	if (binary=="N") {
-		model.train <- lm(trait ~ AFR.prs + EAS.prs + EUR.prs + SAS.prs +age+sex, data=dat_sub.train)
-		y.hat <- predict(model.train, dat_sub.valid)
-		print((cor(y.hat, dat_sub.valid$trait, use="complete.obs"))^2)
-	}
-	if (binary=="Y") {
-		model.train <- glm(trait ~ AFR.prs + EAS.prs + EUR.prs + SAS.prs +age+sex, data=dat_sub.train)
-		y.hat <- predict(model.train, dat_sub.valid)
-		dfroc <- roc(dat_sub.valid$trait, y.hat)
-		print(plot(dfroc,col="red", legacy.axes=TRUE, print.auc=TRUE, print.thres=TRUE, grid=c(0.2,0.2),grid.col=c("blue","yellow")))
-		print(auc(dfroc))
+	dat[[paste0(r, '.prs')]] = std(dat[[paste0(trait, '.', r, '.score_sum')]]) # 可尝试去掉std()
+}
+for (i in 1:pcs) {
+  PCi <- paste0("PC", i)
+  dat[[PCi]] <- std(residuals(lm(as.formula(paste0(PCi, " ~ AFR.prs + EAS.prs + EUR.prs + SAS.prs + age + sex")), na.action=na.exclude, data=dat)))
+}
+# dat <- dat %>% drop_na(race, trait, EUR.prs) %>% group_by(race) %>% slice_sample(n=1000) 
+for (r in races){
+	dat_sub <- subset(dat, race==r)
+	for (i in 1:pcs) {
+		assign(paste0("PC", i, ".center.", r), median(dat_sub[[paste0("PC", i)]],na.rm=TRUE)) # 🐕 必须加上na.rm
 	}
 }
-for (r in races){ 
-	eval(parse(text=paste0( 'dat$d2.', r, ' <- sqrt((dat$PC1 - PC1.center.', r, ')^2 + (dat$PC2 - PC2.center.', r, ')^2)' )))
-	eval(parse(text=paste0( 'dat$d2inv.', r, ' <- 1 / dat$d2.', r )))
+for (r in races){
+	dat$d1 <- 0
+	for (i in 1:pcs) {
+		eval(parse(text = paste0('dat$d1 <- dat$d1 + (dat$PC',i, ' - PC',i,'.center.',r,')^2'))) 
 	}
-	dat$dtot <- dat$d2inv.AFR + dat$d2inv.EAS + dat$d2inv.EUR + dat$d2inv.SAS
+	eval(parse(text=paste0( 'dat$d2.', r, ' <- sqrt(dat$d1)' ))) 
+	eval(parse(text=paste0( 'dat$d2inv.', r, ' <- 1 / dat$d2.', r )))
+}
+dat$dtot <- dat$d2inv.AFR + dat$d2inv.EAS + dat$d2inv.EUR + dat$d2inv.SAS
 for (r in races){
 	eval(parse(text=paste0( 'dat$', r, '.prs_adj <- dat$d2inv.', r, ' / dat$dtot * dat$', r, '.prs' )))
-	}
-for (r in races){
-	print(paste("PROCESS", r))
-	dat_sub <- subset(dat, race==r)
-	dat_sub$prs.sameRace <- std(dat_sub[[paste0(r,".prs")]])
-	ii <- sort(sample(1:nrow(dat_sub), round(nrow(dat_sub)*0.5)))
-	dat_sub.train <- dat_sub[ii,]
-	dat_sub.valid <- dat_sub[-ii,]
-	if (binary=="N") {
-		model.train <- lm(trait ~ AFR.prs_adj + EAS.prs_adj + EUR.prs_adj + SAS.prs_adj +age+sex, data=dat_sub.train)
-		y.hat <- predict(model.train, dat_sub.valid)
-		print((cor(y.hat, dat_sub.valid$trait, use="complete.obs"))^2)
-	}
-	if (binary=="Y") {
-		model.train <- glm(trait ~ AFR.prs_adj + EAS.prs_adj + EUR.prs_adj + SAS.prs_adj +age+sex, data=dat_sub.train)
-		y.hat <- predict(model.train, dat_sub.valid)
-		dfroc <- roc(dat_sub.valid$trait, y.hat)
-		print(plot(dfroc,col="red", legacy.axes=TRUE, print.auc=TRUE, print.thres=TRUE, grid=c(0.2,0.2),grid.col=c("blue","yellow")))
-		print(auc(dfroc))
-	}
 }
 
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 进行拟合 regression
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+if (model=="lm") {dat$trait=dat[[trait]]
+} else if (model=="glm") {dat$trait=ifelse(is.na(dat[[paste0('icdDate_',trait)]]), 0, 1)
+} else if (model=="glm") {
+	dat <- dat %>% mutate(
+		Y_date = dat[[Y]], Y_yes = ifelse( is.na(Y_date), 0,1),
+		follow_end_day = fifelse(!is.na(Y_date), Y_date, fifelse(!is.na(date_lost), date_lost, fifelse(!is.na(date_death), date_death, as.Date("2021-12-31")))),
+		follow_years = (as.numeric(follow_end_day) - as.numeric(date_attend)) / 365.25,
+	) %>% filter( follow_years >0 )
+}
+for (r in races){
+	print(paste("PROCESS", r))
+	auc_C <- list()
+	auc_G <- list()
+	auc_folds_C <- numeric(folds)
+	auc_folds_G <- numeric(folds)
+	dat_sub <- subset(dat, race==r)
+	for (i in 1:folds) { 
+		ii <- sort(sample(1:nrow(dat_sub), round(nrow(dat_sub)*0.9)) )
+		dat_sub.train <- dat_sub[ii,]
+		dat_sub.valid <- dat_sub[-ii,]
+		if (model=="lm") { 
+			model_C.train <- lm(trait ~ AFR.prs + EAS.prs + EUR.prs + SAS.prs +age+sex, data=dat_sub.train)
+			model_G.train <- lm(trait ~ AFR.prs_adj + EAS.prs_adj + EUR.prs_adj + SAS.prs_adj +age+sex, data=dat_sub.train)
+			y_C.hat <- predict(model_C.train, dat_sub.valid)
+			y_G.hat <- predict(model_G.train, dat_sub.valid)
+			print((cor(y_C.hat, dat_sub.valid$trait, use="complete.obs"))^2)
+		} else if (model=="glm") {
+			model_C.train <- glm(trait ~ AFR.prs + EAS.prs + EUR.prs + SAS.prs +age+sex, data=dat_sub.train)
+			model_G.train <- glm(trait ~ AFR.prs_adj + EAS.prs_adj + EUR.prs_adj + SAS.prs_adj +age+sex, data=dat_sub.train)
+			y_C.hat <- predict(model_C.train, dat_sub.valid)
+			y_G.hat <- predict(model_G.train, dat_sub.valid)
+			auc_folds_C[i] <- auc(roc(dat_sub.valid$trait, y_C.hat))
+			auc_folds_G[i] <- auc(roc(dat_sub.valid$trait, y_G.hat))
+		} else if (model=="coxph") {
+			surv.obj <- Surv(time=dat$follow_years, event=dat$Y_yes)
+			model_C.train <- 1-summary(survfit(XXX, newdata=trainset), times=365.25*3)$surv[1,]
+			model_G.train <- 1-summary(survfit(xxx, newdata=trainset), times=365.25*3)$surv[1,]
+			library(nricens)
+			xxx <- get.risk.coxph(xxx, 365.25*3)
+		}
+	}
+	auc_C[[r]] <- auc_folds_C
+	auc_G[[r]] <- auc_folds_G
+}
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 画图
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+auc_C <- as.data.frame(auc_C)
+auc_G <- as.data.frame(auc_G)
+auc_C$Method <- "PRS-CSx"
+auc_G$Method <- "PRS-GRID"
+combined_data <-  rbind(auc_C, auc_G)
+long_data <- pivot_longer(combined_data,  cols = c("EUR", "SAS", "AFR", "EAS"),   names_to = "Race", values_to = "value")
+summary_stats <- long_data %>% group_by(Race, Method) %>% summarize(mean = mean(value), sd = sd(value))
+F1 <- ggplot(summary_stats, aes(x = fct_relevel(Race, "EUR", "AFR", "SAS", "EAS"), y = mean, fill = fct_relevel(Method, "PRS-CSx", "PRS-GRID",))) +
+	geom_bar(stat = "identity", position = position_dodge(), linewidth = 0.8, color = "white", alpha = 0.8) +
+	geom_text(aes(label=round(summary_stats$mean,3)),size=3.5,vjust=7,position =position_dodge(0.9))+
+	geom_errorbar(aes(ymin = mean - sd, ymax = mean + sd), width = 0.2, position = position_dodge(0.9), linewidth = 0.8, color = "grey50") +
+	labs(title = "Mean AUC with SD given by 10-fold cross validation", x = "Race", y = "Mean Value", fill = "Method") +
+	theme_bw() +
+	theme(plot.tag = element_text(size = 15), axis.text.y = element_text(size = 12), axis.text.x = element_text(size = 12), axis.title.y= element_text(size = 15), strip.text = element_text(size = 12))+scale_fill_manual(values = c("grey70","#4DBBD5FF"))
+tiff(file = "Figure2.tiff", compression="lzw",width = 6000, height = 4000, res = 600)
+dev.off()
+#dat <- read_excel("PRS.xlsx",sheet="Sheet2")
+#barplot(t(as.matrix(dat[, 2:3])), beside = TRUE, names.arg = dat$人种, main="两种方法对height的预测", legend.text = TRUE,
+#	ylim = c(0,0.15), col = c("green", "blue"), ylab = expression(italic("Nagelkerke R2")), xlab = "人种")
+	
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Sanity check on basic stuff
@@ -92,22 +143,4 @@ myhist <- hist(dat$prs, breaks=20)
 	avg <- by(dat$trait, cut(dat$prs, breaks=myhist$breaks), function(x) mean(x,na.rm=T))
 	par(new=T); plot(myhist$mids, avg, pch=16, axes=F, xlab=NA, ylab=NA, cex=1.2)
 	axis(side=4); mtext(side=4, line=3, 'measured')	
-
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# PRS对比结果展示图
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-setwd("C:/Users/jiehu/Desktop")
-pacman::p_load(readxl, tidyverse, ggplot2)
-dat <- read_excel("PRS.xlsx",sheet="Sheet2")
-barplot(t(as.matrix(dat[, 2:3])), 
-        beside = TRUE,
-        names.arg = dat$人种,
-		main="两种方法对height的预测",
-        legend.text = TRUE,
-		ylim = c(0,0.15),
-		col = c("green", "blue"),
-        ylab = expression(italic("Nagelkerke R2")),
-        xlab = "人种")
-
 
