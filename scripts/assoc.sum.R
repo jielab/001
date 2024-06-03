48cd02228da90c002fc6f54bcf8a3129c34648ce
Jie Huang <jiehuang001@gmail.com>
2024-06-03 11:54:33 +0800

    Update assoc.sum.R

--- scripts/assoc.sum.R
+++ scripts/assoc.sum.R
@@ -1,21 +1,26 @@
-pacman::p_load(tidyr, TwoSampleMR, MendelianRandomization, survival, survminer)
+pacman::p_load(data.table, stringi, tidyr, TwoSampleMR, MendelianRandomization, survival, survminer)
+
+pattern=c('^snp$|^rsid$|variant_id', '^chr$|^chrom', '^bp$|^pos$|^position|^base_pair', '^ea$|^alt$|^a1$|^effect_allele$', '^nea$|^ref|^allele0$|^a2$|^other_allele', '^eaf$|^a1freq$|^effect_allele_freq', '^n$|^Neff$', '^beta$|^effect$', '^se$|^standard_error', '^p$|^pval$|^p_bolt_lmm')
+replacement=c('SNP', 'CHR', 'POS','EA', 'NEA', 'EAF', 'N', 'BETA', 'SE', 'P')
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # 基于MR的mediation
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-dir.Y <- dir.X = 'D:/data/gwas/main'
-dir.M <- "D:/data/gwas/bb"
-Xs <- "?"
-Ys <- "?" # grep('^y.', names(XYs), value=T)
-Ms <- "?" # c('bb_CRE', 'bb_ALB', 'bb_ALP', 'bb_ALT', 'bb_APOA', 'bb_APOB', 'bb_CHOL', 'bb_CRE', 'bb_CRP', 'bb_CYS', 'bb_EGFR', 'bb_LPA', 'bb_SHBG', 'bb_TES', 'bb_VITD')
+dir.Y = dir.X = '/data/sph-huangj/data/gwas/main'
+dir.M = '/data/sph-huangj/data/gwas/bb'
+Xs = '?'
+Ys = '?' 
+Ms = gsub('.gz', '', list.files(path=dir.M, pattern='.gz$')); Ms
 sink("?.log")
 
 for (Y in Ys) {
-	writeLines(paste('\n\n\t-->Run:', Y))
+	writeLines(paste('\n\n-->Run:', Y))
 	dat.Y.raw <- read.table(paste0(dir.Y, '/', Y, '.gz'), header=T)
+	names(dat.Y.raw) <- stri_replace_all_regex(toupper(names(dat.Y.raw)), pattern=toupper(pattern), replacement=replacement, vectorize_all=FALSE)
 
 	for (X in Xs) { # X
-		dat.X.raw <- read.table(paste0(dir.X, '/', X, '.gz'), header=T) 
+		dat.X.raw <- read.table(paste0(dir.X, '/', X, '.gz'), header=T)
+		names(dat.X.raw) <- stri_replace_all_regex(toupper(names(dat.X.raw)), pattern=toupper(pattern), replacement=replacement, vectorize_all=FALSE)
 		if (file.exists(paste0(dir.X, '/', X, '.top.snp'))) {
 			dat.X.iv <- read.table(paste0(dir.X, '/', X, '.top.snp'), header=T); names(dat.X.iv) <- "SNP" 
 		} else {
@@ -23,14 +28,15 @@ for (Y in Ys) {
 		}
 		
 		for (M in Ms) { # M
-			writeLines(paste('\t\tRun:', Y, X, M))
-			dat.M.raw <- read.table(paste0(dir.M, '/', M, '.gz'), header=T) 
+			writeLines(paste('\n\tRun:', Y, X, M))
+			dat.M.raw <- read.table(paste0(dir.M, '/', M, '.gz'), header=T)
+			names(dat.M.raw) <- stri_replace_all_regex(toupper(names(dat.M.raw)), pattern=toupper(pattern), replacement=replacement, vectorize_all=FALSE)
 			if (file.exists(paste0(dir.M, '/', M, '.top.snp'))) {
 				dat.M.iv <- read.table(paste0(dir.M, '/', M, '.top.snp'), header=T); names(dat.M.iv) <- "SNP" 
 			} else {
 				dat.M.iv <- dat.M.raw %>% filter(P<5e-8) %>% dplyr::select(SNP)
 			}
-			
+
 			# 合并 dat.X 和 dat.M 
 			dat.XnM.iv <- rbind(dat.X.iv, dat.M.iv) %>% unique() # 最理想的是把 dat.X和dat.M中所有的显著性snp合并然后 %>% clump_data() %>% select(SNP)
 			dat.X <- dat.X.raw %>% merge(dat.X.iv) %>% format_data(type='exposure', snp_col='SNP', effect_allele_col='EA', other_allele_col='NEA', beta_col='BETA', se_col='SE', pval_col='P') %>% mutate(id.exposure=X)
@@ -41,7 +47,7 @@ for (Y in Ys) {
 			dat.XM <- harmonise_data(dat.X, dat.M.4X, action=1)
 			dat.XM.mv <- harmonise_data(dat.X.mv, dat.M.mv, action=1); names(dat.XM.mv) <- gsub("outcome", "mediator", names(dat.XM.mv))
 			res.X2M <- mr(dat.XM, method_list=c("mr_wald_ratio", "mr_ivw")); res.X2M 
-			beta.X2M <- res.X2M %>% pull(b); se.X2M <- res.X2M %>% pull(se); p.X2M <- signif(res.X2M %>% pull(pval),2)
+			beta.X2M <- res.X2M$b; se.X2M <- res.X2M$se; p.X2M <- signif(res.X2M$pval,2)
 	
 			# 合并 dat.X 和 dat.M 和 dat.Y
 			dat.Y <- merge(dat.Y.raw, dat.XnM.iv, by="SNP") %>% format_data(type='outcome', snp_col='SNP', effect_allele_col='EA', other_allele_col='NEA', beta_col='BETA', se_col='SE', pval_col='P') %>% mutate(id.outcome =Y)		
@@ -54,18 +60,18 @@ for (Y in Ys) {
 			# Total effect
 			dat.XY <- harmonise_data(dat.X, dat.Y, action=1) 
 			res.X2Y <- mr(dat.XY, method_list=c("mr_wald_ratio", "mr_ivw")); res.X2Y 
-			beta.X2Y <- res.X2Y %>% pull(b); se.X2Y <- res.X2Y %>% pull(se); p.X2Y <- signif(res.X2Y %>% pull(pval),2)	
+			beta.X2Y <- res.X2Y$b; se.X2Y <- res.X2Y$se; p.X2Y <- signif(res.X2Y$pval,2)
 			
 			# Step 2: M (+X) --> Y 
 			dat.mvmr <- MendelianRandomization::mr_mvinput(bx=cbind(dat$beta.mediator, dat$beta.exposure), bxse=cbind(dat$se.mediator, dat$se.exposure), by=dat$beta.outcome, byse=dat$se.outcome)
 			res.mvmr <- MendelianRandomization::mr_mvivw(dat.mvmr, model="default", correl=FALSE, distribution="normal", alpha=0.05); res.mvmr
-			beta.M2Y <- as.numeric(res.mvmr$Estimate[1]); se.M2Y <- as.numeric(res.mvmr$StdError[1]); p.M2Y <- as.numeric(res.mvmr$Pvalue[1])
+			beta.M2Y <- res.mvmr$Estimate[["Bx1"]]; se.M2Y <- res.mvmr$StdError[["Bx1"]] ; p.M2Y <- res.mvmr$Pvalue[["Bx1"]]
 			
 			# Mediation
 			beta.me <- beta.X2M * beta.M2Y # 🐕 乘法 
 			se.me <- sqrt(beta.X2M^2 * se.X2M^2 + beta.M2Y^2 * se.M2Y^2) # 🐕 POE法，也称Delta法 
 			p.me <- 2*pnorm(-abs(beta.me/se.me))
-			print(paste("RES:", X,M,Y, round(beta.X2Y,3),round(se.X2Y,3),p.X2Y, round(beta.X2M,3),round(se.X2M,3),p.X2M, round(beta.M2Y,3),round(se.M2Y,3),p.M2Y, round(beta.me,3),round(se.me,3),p.me))
+			print(paste("RES:", X,M,Y, round(beta.X2Y,3),round(se.X2Y,3),p.X2Y, round(beta.X2M,3),round(se.X2M,3),p.X2M, round(beta.M2Y,3),round(se.M2Y,3),signif(p.M2Y,2), round(beta.me,3),round(se.me,3),signif(p.me,2)))
 
 		}	
 	}
