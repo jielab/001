82be9593d16ea4996c27c8c525ce2e89f474c2bb
Jie Huang <jiehuang001@gmail.com>
2024-07-04 18:22:30 +0800

    Create mrMe-demo.R

---
+++ scripts/mrMe-demo.R
@@ -0,0 +1,124 @@
+setwd("C:/Users/jiehu/Desktop")
+# devtools::install_github("scllin/mrMed")
+pacman::p_load(data.table, stringi, dplyr, tidyverse, TwoSampleMR, MendelianRandomization, mrMed, RadialMR, psych, bda)
+
+pattern=c('^snp$|^rsid$', '^chr$|^chrom$|^chromosome$', '^bp$|^pos$|^position$|^base_pair_location$', '^ea$|^alt$|^a1$|^effect_allele$', '^nea$|^ref|^allele0$|^a2$|^other_allele$', '^eaf$|^a1freq$|^effect_allele_frequency$', '^n$|^Neff$', '^beta$|^effect$', '^se$|^standard_error', '^p$|^pval$|^p_value$')
+replacement=c('SNP', 'CHR', 'POS', 'EA', 'NEA', 'EAF', 'N', 'BETA', 'SE', 'P')
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# TwoSampleMR最简单的方法
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+iv.file <- 'D:/data/gwas/main/ppp_ABO.top.snp'
+dat.X.file <- 'D:/data/gwas/main/ppp_ABO.gz'
+dat.Y.file <- 'D:/data/gwas/main/covid_B2.EUR.gz'
+dat.X.raw <- read.table(dat.X.file, header=T)
+	names(dat.X.raw) <- stri_replace_all_regex(toupper(names(dat.X.raw)), pattern=toupper(pattern), replacement=replacement, vectorize_all=FALSE)
+	if (file.exists(iv.file)) {
+		dat.X.iv <- read.table(iv.file, header=T); names(dat.X.iv) <- "SNP" 
+	} else {
+		dat.X.sig <- dat.X.raw %>% filter(P<=5e-08) %>% mutate(mb=ceiling(POS/1e+05))
+		dat.X.iv <- dat.X.sig %>% group_by(mb) %>% slice(which.min(P)) %>% ungroup() %>% select("SNP")
+	}
+	dat.X <- dat.X.raw %>% merge(dat.X.iv, by="SNP") %>% 
+	format_data(type='exposure', snp_col='SNP', effect_allele_col='EA', other_allele_col='NEA', beta_col='BETA', se_col='SE', pval_col='P') 
+dat.Y.raw <- read.table(dat.Y.file, header=T)
+	names(dat.Y.raw) <- stri_replace_all_regex(toupper(names(dat.Y.raw)), pattern=toupper(pattern), replacement=replacement, vectorize_all=FALSE)
+ 	dat.Y <- dat.Y.raw %>% merge(dat.X.iv, by="SNP") %>% 
+	format_data(type='outcome', snp_col='SNP', effect_allele_col='EA', other_allele_col='NEA', beta_col='BETA', se_col='SE', pval_col='P') 
+dat <- harmonise_data(dat.X, dat.Y, action=1) 
+dat.radial <- format_radial(dat$beta.exposure, dat$beta.outcome, dat$se.exposure, dat$se.outcome, dat$SNP)
+	ivw.radial <- ivw_radial(dat.radial, 0.05/nrow(dat.radial), 3, 0.0001)
+	egg.radial <- egger_radial(dat.radial, 0.05/nrow(dat.radial), 3); egg.radial$outliers
+	plot_radial(c(ivw.radial, egg.radial), T, F, F)
+res <- mr(dat); res; # generate_odds_ratios(res) # 置信区间
+	mr(dat, method_list = c("mr_ivw", "mr_egger_regression", "mr_egger_regression_bootstrap"))
+	mr_pleiotropy_test(dat); mr_heterogeneity(dat)
+	mr_scatter_plot(res, dat)[[1]]
+	res.single <- mr_singlesnp(dat)
+	mr_forest_plot(res.single)[[1]]
+	mr_funnel_plot(res.single)
+dat.bsmr <- dat_to_MRInput(dat); dat.bsmr <- dat.bsmr[[1]]
+	mr_plot(dat.bsmr, interactive=F)
+	mr_plot(mr_allmethods( dat.bsmr, method="main", iterations = 50)) # method="all"
+	mr_forest(dat.bsmr, snp_estimates=F, methods=c("ivw", "median", "wmedian", "egger", "maxlik", "conmix")) 
+	+ scale_colour_manual(values = c("IVW estimate"="red")) + theme(axis.title.y=element_text(size=20, face="bold"), axis.title.x=element_text(size=10, face="bold"), axis.text.x=element_text(size=10, face="bold"), axis.text.y=element_text(size=15, face="bold"))
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# MR：从个体数据到summary数据
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# MR的关键词是 genetically determined X。
+# 对于一个很强的SNP或PRS，它基本就可代表“X本身”；但对于一个很弱的，它所决定的X，跟X本身差别很大。
+library(OneSampleMR)
+dat0 <- readRDS(file="D:/data/ukb/Rdata/all.Rdata")
+dat <- dat0 %>% filter(ethnic_cat=="White") %>% rename(G=walk_pace.score_sum)
+dat1$X.pred = predict.lm( lm( X ~ G, data=dat1)) 
+summary(lm(Y ~ X.pred, dat1))
+fit1 <- ivreg::ivreg(Y ~ X | G, data = dat1); summary(fit1) # 跟上面的结果一样。 Z可以是 G1+G2+G3
+#下面展示基于summary数据的方法，所得结果也是一样
+	beta.G2X = summary(lm( X ~ G, data=dat1))$coef[2,1]
+	beta.G2Y = summary(lm( Y ~ G, data=dat1))$coef[2,1]
+	se.G2X = summary(lm( X ~ G, data=dat1))$coef[2,2]
+	se.G2Y = summary(lm( Y ~ G, data=dat1))$coef[2,2]
+	beta.wald = beta.G2Y / beta.G2X; beta.wald
+	se.wald = se.G2Y / beta.G2X; se.wald
+	signif(2*pnorm(-abs(beta.wald/se.wald)), 2)
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# mediation：从个体数据到summary数据
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 示例原文出处：https://rpubs.com/MichaelGilbertUCR/732775
+# 同时参考https://rdrr.io/github/scllin/toypack/src/R/mrMed.R
+set.seed(12334) 
+dat1 <- iris %>% rename(X=Sepal.Length) %>% 
+	mutate(
+		random1 = runif(nrow(iris), min=min(X), max=max(X)),
+		M = X*0.35+random1*0.65,
+		random2 = runif(nrow(iris), min=min(M), max=max(M)),
+		Y = M*0.35 + random2*.65,
+		Y_yes = c(rep(c(0,0,1),15), rep(0:1,30), rep(c(1,1,0),15)),
+		G1 = ifelse(X > quantile(X, probs=0.80), 2, ifelse(X < quantile(X, probs=0.20), 0, 1)), # a strong SNP
+		G2 = ifelse(X > quantile(X, probs=0.98), 2, ifelse(X < quantile(X, probs=0.02), 0, 1))  # a weak SNP		
+) %>% dplyr::select(X, M, Y, Y_yes, G1, G2) 
+	dat1 %>% psych::pairs.panels()
+	psych::mediate(y="Y", x="X", m="M", data=dat1, n.iter=1000) %>% print(short=FALSE) # 🎉 其实，这一行代码就可以了。
+	bda::mediation.test(dat1$M, dat1$X, dat1$Y)
+fit.X2Y <- lm(Y ~ X, data=dat1); summary(fit.X2Y)
+	coef.X2Y <- coef(summary(fit.X2Y)); coef.X2Y 
+	beta.X2Y <- coef.X2Y[2,1]; se.X2Y <- coef.X2Y[2,2]; p.X2Y <- signif(coef.X2Y[2,4],2)
+fit.X2M <- lm(M ~ X, data=dat1); summary(fit.X2M)
+	coef.X2M <- coef(summary(fit.X2M)); coef.X2M
+	beta.X2M <- coef.X2M[2,1]; se.X2M <- coef.X2M[2,2]; p.X2M <- signif(coef.X2M[2,4],2)
+fit.M2Y <- lm(Y ~ M+X, data=dat1); summary(fit.M2Y) # 🐖 这儿必须捎带上X。 X的BETA=0.01667，是ADE，不再显著，表明 complete mediation。
+	coef.M2Y <- coef(summary(fit.M2Y)); coef.M2Y 
+	beta.M2Y <- coef.M2Y[2,1]; se.M2Y <- coef.M2Y[2,2]; signif(coef.M2Y[2,4],2)
+	beta.X2Y.dr <- coef.M2Y[3,1]; se.X2Y.dr <- coef.M2Y[3,2]; signif(coef.M2Y[3,4],2)
+res <- mediation::mediate(fit.X2M, fit.M2Y, treat="X", mediator="M", boot=T); print(SUM <- summary(res))
+	c(round(SUM$d.avg,3), round(SUM$z.avg,3), round(SUM$tau.coef,3), round(SUM$n.avg,3)) # ACME, ADE, Total, Prop
+	beta.me <- beta.X2M * beta.M2Y; beta.me # 乘法
+		se.me <- sqrt(beta.X2M^2 * se.X2M^2 + beta.M2Y^2 * se.M2Y^2); se.me # 🐕 POE法，也称Delta法 
+		p.me <- signif(2*pnorm(-abs(beta.me/se.me)),2); p.me
+	beta.dr <- beta.X2Y - beta.me; beta.dr # 减法
+		se.dr <- sqrt(se.X2Y^2 + se.me^2); se.dr
+		p.dr <- signif(2*pnorm(-abs(beta.dr/se.dr)),2); p.dr
+	beta.prop <- beta.me / beta.X2Y; beta.prop
+		se.prop <- sqrt(se.me^2/beta.X2Y^2 + beta.me^2*se.X2Y^2/beta.X2Y^4); se.prop
+		p.prop <- signif(2*pnorm(-abs(beta.prop/se.prop)),2); p.prop
+	
+	
+	head(WHR_T2D_CAD)
+"SNP"             "Gx"              "Gm"              "Gx_plum"         "Gm_plum"         "G_mvmr"         
+ [7] "effect_allele.X" "other_allele.X"  "eaf.X"           "beta.X"          "se.X"            "pval.X"         
+[13] "effect_allele.M" "other_allele.M"  "eaf.M"           "beta.M"          "se.M"            "pval.M"         
+[19] "effect_allele.Y" "other_allele.Y"  "eaf.Y"           "beta.Y"          "se.Y"            "pval.Y"   
+	mrMed(WHR_T2D_CAD)
+	
+	
+
+	X_str=paste0(X, "(", nrow(dat.X.iv), ")"); M_str=paste0(M, "(", nrow(dat.M.iv), ")");
+			print(paste("RES:", X_str, M_str, Y, nrow(dat.XY), round(beta.X2Y,3), round(se.X2Y,3), signif(p.X2Y,2), "|", 
+				nrow(dat.XM), round(beta.X2M,3), round(se.X2M,3), signif(p.X2M,2), "|", 
+				nrow(dat), round(beta.M2Y,3), round(se.M2Y,3), signif(p.M2Y,2), round(beta.X2Y.dr,3), round(se.X2Y.dr,3), signif(p.X2Y.dr,2), "|", 
+				round(beta.me,3), signif(p.me,2),  round(beta.dr,3), signif(p.dr,2), NA,NA, round(beta.prop,3), signif(p.prop,2)  # ACME, ADE, TOT, Prop
+			))
