5b490758118aeb30b4c4fe16db9cbd146b5129c9
Jie Huang <jiehuang001@gmail.com>
2024-11-25 12:19:30 +0800

    JH update

--- scripts/assoc.sum.R
+++ scripts/assoc.sum.R
@@ -1,73 +1,35 @@
 # devtools::install_github("scllin/mrMed") 
 pacman::p_load(data.table, tidyverse, stringi, TwoSampleMR, MendelianRandomization, mrMed, cisMRcML) # foreach, doParallel
-pattern=c('^snp$|^rsid$', '^chr$|^chrom$|^chromosome$', '^bp$|^pos$|^position$|^base_pair_location$', '^ea$|^alt$|^a1$|^effect_allele$', '^nea$|^ref|^allele0$|^a2$|^other_allele$', '^eaf$|^a1freq$|^effect_allele_frequency$', '^n$|^Neff$', '^beta$|^effect$', '^se$|^standard_error', '^p$|^pval$|^p_value$')
-replacement=c('SNP', 'CHR', 'POS', 'EA', 'NEA', 'EAF', 'N', 'BETA', 'SE', 'P')
-pval <- function(b,se) (2*pnorm(-abs(b/se)))
-rb <- function(x) (round(x,3)); rp <- function(x) (signif(x,2))
+dir0='/work/sph-huangj/'
+source(paste0(dir0, 'scripts/main/f.R'))
+dir=paste0(dir0, 'data/gwas/')
 
-dir='/work/sph-huangj'
-dir.X = paste0(dir,'/data/gwas/?/clean')
-dir.Y = paste0(dir,'/data/gwas/?/clean')
-dir.M = paste0(dir,'/data/gwas/?/clean')
-cis=0
-Xs = '?' # 一个X，或全部 ALL
-Ys = '?' # 一个X，或全部 ALL
-label='?'; log_mr=paste0(label,'.mr.log'); log_mrMed=paste0(label,'.mrMed.log')
+n_cores=40
+folder.X = '?'
+folder.Y = '?'
+folder.M = '?' # 有可能有多个folders，用|分割
+Xs = '?' # 一个X，或 ALL 表示全部
+Ys = '?' # 一个Y，或 ALL 表示全部
+label='?'; log_mr=paste0(label,'.mr.log')
+
+cis=?
+cisMRcML=?; log_cisMrcML=paste0(label,'.cisMrcML.log')
+coloc=?; log_coloc=paste0(label,'.coloc.log')
+mrMed=?; log_mrMed=paste0(label,'.mrMed.log')
+
+
+dir.X = paste0(dir,folder.X,'/clean')
+dir.Y = paste0(dir,folder.Y,'/clean')
 if (grepl("ALL", Xs)) {Xs = gsub('.gz', '', list.files(path=dir.X, pattern='.gz$'))}
 if (grepl("ALL", Ys)) {Ys = gsub('.gz', '', list.files(path=dir.Y, pattern='.gz$'))}
-if (!grepl("NA", dir.M)) {Ms = gsub('.gz', '', list.files(path=dir.M, pattern='.gz$'))}
-n_cores=40
-
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# 中介function
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-mediation <- function(X, dat.X.raw, dat.X.iv, M, Y, dat.Y.raw, log_mrMed) {
-	# Harmonize dat.X + dat.M + dat.Y
-	dat.M.raw <- read.table(paste0(dir.M, '/', M, '.gz'), header=T)
-	dat.M.4x <- dat.M.raw %>% merge(dat.X.iv) %>% format_data(type='outcome', snp_col='SNP',  effect_allele_col='EA', other_allele_col='NEA', beta_col='BETA', se_col='SE', pval_col='P') %>% mutate(id.outcome=M)
-	names(dat.M.raw) <- stri_replace_all_regex(toupper(names(dat.M.raw)), pattern=toupper(pattern), replacement=replacement, vectorize_all=FALSE)
-	if (file.exists(paste0(dir.M, '/', M, '.top.snp'))) {
-		dat.M.iv <- read.table(paste0(dir.M, '/', M, '.top.snp'), header=T); names(dat.M.iv) <- 'SNP' 
-	} else if (file.exists(paste0(dir.M, '/', M, '.NEW.top.snp'))) {
-		dat.M.iv <- read.table(paste0(dir.M, '/', M, '.NEW.top.snp'), header=T)
-	} else {
-		dat.M.sig <- dat.M.raw %>% filter(P<=5e-08) %>% mutate(mb=ceiling(POS/1e+05))
-		dat.M.iv <- dat.M.sig %>% group_by(mb) %>% slice(which.min(P)) %>% ungroup() %>% select('SNP')
-		write.table(dat.M.iv, paste0(dir.M, '/', M, '.NEW.top.snp'), append=FALSE, quote=FALSE, row.names=FALSE, col.names=TRUE)
+Ms.full = NULL # 如果 folder.M 不是 NA，后面会自动给 Ms 赋值
+if (!grepl("NA", folder.M)) {
+	for (folder.m in str_split_1(folder.M, ' ')) {
+		Ms.full <- append(Ms.full, list.files(path=paste0(dir,folder.m,'/clean'), pattern='.gz$', full.names=TRUE))	
 	}
-	dat.XnM.iv <- rbind(dat.X.iv, dat.M.iv) %>% unique() # 最理想的是把 dat.X和dat.M中所有的显著性snp合并然后 %>% clump_data() %>% select(SNP)
-	dat.X.mv <- dat.X.raw %>% merge(dat.XnM.iv) %>% format_data(type='exposure', snp_col='SNP', effect_allele_col='EA', other_allele_col='NEA', beta_col='BETA', se_col='SE', pval_col='P') %>% mutate(id.exposure=X)
-	dat.M.mv <- dat.M.raw %>% merge(dat.XnM.iv) %>% format_data(type='outcome',  snp_col='SNP', effect_allele_col='EA', other_allele_col='NEA', beta_col='BETA', se_col='SE', pval_col='P') %>% mutate(id.outcome =M)
-	dat.Y.mv <- dat.Y.raw %>% merge(dat.XnM.iv) %>% format_data(type='outcome',  snp_col='SNP', effect_allele_col='EA', other_allele_col='NEA', beta_col='BETA', se_col='SE', pval_col='P') %>% mutate(id.outcome =Y)		
-	dat.XY.mv <- harmonise_data(dat.X.mv, dat.Y.mv, action=1)
-	dat.XM.mv <- harmonise_data(dat.X.mv, dat.M.mv, action=1); names(dat.XM.mv) <- gsub('outcome', 'mediator', names(dat.XM.mv))
-	dat <- merge(dat.XM.mv, dat.XY.mv, by='SNP')
-	bad_row <- subset(dat, effect_allele.exposure.x != effect_allele.exposure.y) %>% nrow()
-	if (bad_row !=0) { write(paste("ERR:", X, Y, M, "X-M-Y have inconsistent alleles !!"), file=log_mrMed, append=TRUE); break }
-	names(dat) <- gsub("\\.x$", "", names(dat)); dat <- subset(dat, select=!grepl("\\.y", names(dat)))
-	if (nrow(dat)==0) { write(paste("SKIP:", X, Y, M, "X-M-Y harmonized data is empty !!"), file=log_mrMed, append=TRUE); break }
-	# mrMed 方法 🏮🏮
-	dat1 <- dat
-	names(dat1) <- stri_replace_all_regex(names(dat1), pattern=c("exposure", "mediator", "outcome"), replacement=c("X", "M", "Y"), vectorize_all=FALSE)
-	dat1 <- dat1 %>% mutate (
-		Gx = ifelse(dat$SNP %in% dat.X.iv$SNP, 1, 0), Gx_plum = Gx, # 可加上 “& pval.M >5e-08”, 用于 X -> M或Y
-		Gm = 1, Gm_plum = Gm, # 可考虑 ifelse(dat$SNP %in% dat.M.iv$SNP, 1, 0), 用于 M -> Y 
-		G_mvmr = ifelse(Gx_plum==0 & Gm_plum==0, 0, 1)
-	)
-	if ( max(dat1$Gx) * max(dat1$Gm) ==0 ) {
-		mrMed_str <- paste("mrMed has no SNP !!")
-	} else {
-		res <- mrMed(dat_mrMed=dat1, method_list="Prod_IVW_0")
-	}
-	X_str=paste0(X, "(", nrow(dat.X.iv), ")"); M_str=paste0(M, "(", nrow(dat.M.iv), ")");
-	write(paste("RES:", X_str, M_str, Y, "|X2Y", nrow(dat.XY), rb(beta.X2Y), rp(p.X2Y), 
-		"|mrMed", paste(rb(res$TE$b), rp(res$TE$p), rb(res$IE$b), rp(res$IE$p), rb(res$DE$b), rp(res$DE$p), rb(res$rho$b), rp(res$rho$p)) # TE, ACME, ADE, Prop(rho)
-		), file=log_mrMed, append=TRUE
-	)
 }
 
-			
+	
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # 进行分析
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -79,28 +41,11 @@ for (Y in Ys) { # 🙍
 	for (X in Xs) { # 🍷
 		dat.X.raw <- read.table(paste0(dir.X, '/', X, '.gz'), header=T)
 		names(dat.X.raw) <- stri_replace_all_regex(toupper(names(dat.X.raw)), pattern=toupper(pattern), replacement=replacement, vectorize_all=FALSE)
-		if (cis==1) { dat.X.raw <- subset(dat.X.raw, CHR==chr & POS>=(pos_begin-flank) & POS<=(pos_end+flank)) }
-		
-		##🗡 基于某个locus所有SNP进行cisMR-cML分析 🏮🏮
-		if (cisMR==1) {
-			mr_dat$exp_df$cor = mr_dat$exp_df$b / sqrt(mr_dat$exp_df$b^2 + (mr_dat$exp_df$N - 2) * mr_dat$exp_df$se^2)
-			mr_dat$exp_df$bJ = solve(mr_dat$LD_mat) %*% mr_dat$exp_df$cor
-			mr_dat$out_df$cor = mr_dat$out_df$b / sqrt(mr_dat$out_df$b^2 + (mr_dat$out_df$N - 2) * mr_dat$out_df$se^2)
-			mr_dat$out_df$bJ = solve(mr_dat$LD_mat) %*% mr_dat$out_df$cor
-			b_exp_cond=mr_dat$exp_df$bJ
-			b_out_cond=mr_dat$out_df$bJ 
-			Sig_exp1 = solve(mr_dat$LD_mat) %*% (mr_dat$exp_df$se_cor %o% mr_dat$exp_df$se_cor * mr_dat$LD_mat) %*% solve(mr_dat$LD_mat)
-			Sig_out1 = solve(mr_dat$LD_mat) %*% (mr_dat$out_df$se_cor %o% mr_dat$out_df$se_cor * mr_dat$LD_mat) %*% solve(mr_dat$LD_mat)
-			Sig_exp_inv=solve(Sig_exp1)
-			Sig_out_inv=solve(Sig_out1)
-			res = cismr_cML_DP(
-				b_exp=b_exp_cond,b_out=b_out_cond, Sig_exp_inv=Sig_exp_inv,Sig_out_inv=Sig_out_inv,maxit=200, n = mr_dat$N1,random_start = 5,
-                min_theta_range=-0.1,max_theta_range=0.1, num_pert=100,random_start_pert=5,random_seed = 12345
-			)			  
-			c(res$BIC_DP_theta, res$BIC_DP_se, res$BIC_DP_p)
+		if (cis==1) { # 下面所有的分析，只保留X 的 cis-SNP 数据。 所以，只有protein作为X的时候，才设置 cis=1
+			dat.X.raw <- subset(dat.X.raw, CHR==chr & POS>=(pos_begin-flank) & POS<=(pos_end+flank)) 
 		}
-
-		##🗡 基于显著SNP工具变量进行常规MR分析 🏮🏮
+		
+		##🗡 基于显著的SNP，进行常规MR分析 🏮🏮
 		if (file.exists(paste0(dir.X, '/', X, '.top.snp'))) {
 			dat.X.iv <- read.table(paste0(dir.X, '/', X, '.top.snp'), header=T); names(dat.X.iv) <- "SNP" 
 		} else if (file.exists(paste0(dir.X, '/', X, '.NEW.top.snp'))) {
@@ -122,13 +67,46 @@ for (Y in Ys) { # 🙍
 			write("X Y M BETA SE P.ivw p.hetero.egger p.hetero.weighted p.pleio", file=log_mr, append=FALSE)
             write(paste(X,Y, nrow(dat.XY), rb(beta.X2Y), rb(se.X2Y), rp(p.X2Y), rp(p.hetero[1]), rp(p.hetero[2]), rp(p.pleio)), file=log_mr, append=TRUE)
 
-		##🗡 中介分析
-		# next # 🛑 
-		if(p.X2Y >0.05) next
-		# numCores <- detectCores()-1; cl <- makeCluster(numCores); registerDoParallel(cl)
-		# foreach::foreach(M = Ms) %dopar% { # 🐎
-		for (M in Ms) { mediation(X, dat.X.raw, dat.X.iv, M, Y, dat.Y.raw, log_mrMed) }
-		# stopCluster(cl)
+		##🗡 基于某个locus的所有SNP，进行更加精准的cisMR-cML分析 🏮🏮
+		if (cisMRcML==1) {
+			dat$exp_df$cor = dat$exp_df$b / sqrt(dat$exp_df$b^2 + (dat$exp_df$N - 2) * dat$exp_df$se^2)
+			dat$exp_df$bJ = solve(dat$LD_mat) %*% dat$exp_df$cor
+			dat$out_df$cor = dat$out_df$b / sqrt(dat$out_df$b^2 + (dat$out_df$N - 2) * dat$out_df$se^2)
+			dat$out_df$bJ = solve(dat$LD_mat) %*% dat$out_df$cor
+			b_exp_cond=dat$exp_df$bJ
+			b_out_cond=dat$out_df$bJ 
+			Sig_exp1 = solve(dat$LD_mat) %*% (dat$exp_df$se_cor %o% dat$exp_df$se_cor * dat$LD_mat) %*% solve(dat$LD_mat)
+			Sig_out1 = solve(dat$LD_mat) %*% (dat$out_df$se_cor %o% dat$out_df$se_cor * dat$LD_mat) %*% solve(dat$LD_mat)
+			Sig_exp_inv=solve(Sig_exp1)
+			Sig_out_inv=solve(Sig_out1)
+			res = cismr_cML_DP(
+				b_exp=b_exp_cond, b_out=b_out_cond, Sig_exp_inv=Sig_exp_inv, Sig_out_inv=Sig_out_inv, maxit=200,
+				n=dat$N1,random_start=5, min_theta_range=-0.1, max_theta_range=0.1, num_pert=100, random_start_pert=5, random_seed=12345
+			)			  
+			write("X Y m BETA SE P", file=log_cisMrcML, append=FALSE)
+            write(paste(X,Y, nrow(dat), rb(res$BIC_DP_theta), rb(es$BIC_DP_se), rp(res$BIC_DP_p)), file=log_cisMrcML, append=TRUE)
+		}
+		
+		##🗡 对MR显著的locus，进行coloc分析 🏮🏮
+		if (coloc==1 & p.X2Y <=0.05) {
+			dat <- harmonise_data(dat.X, dat.Y, action=1) 
+			dat.coloc.X <- dat %>% {list(type="quant", snp=.$SNP, position=.$pos.exposure, MAF=.$eaf.exposure, N=.$samplesize.exposure, beta=.$beta.exposure, varbeta =.$se.exposure^2, pvalues=.$pval.exposure)}
+			dat.coloc.Y <- dat %>% {list(type="cc", snp=.$SNP, position=.$pos.outcome, MAF=.$eaf.outcome, N=100000, beta=.$beta.outcome, varbeta =.$se.outcome^2, pvalues=.$pval.outcome)}
+			fit.coloc <- coloc::coloc.abf(dat.coloc.X, dat.coloc.Y)
+			t(as.data.frame(fit.coloc[["summary"]])) 
+			par(mfrow = c(2,1)); plot_dataset(dat.coloc.X); plot_dataset(dat.coloc.Y)
+			sensitivity(fit.coloc,"H4>0.9")
+		}
+		
+		##🗡 对MR显著的locus，进行中介分析 🏮🏮
+		if(mrMed==1 & p.X2Y <=0.05) {
+			# numCores <- detectCores()-1; cl <- makeCluster(numCores); registerDoParallel(cl)
+			# foreach::foreach(M = Ms) %dopar% {
+			for (file.M in Ms.full) {
+				mediation(X, dat.X.raw, dat.X.iv, file.M, Y, dat.Y.raw, log_mrMed) 
+			}
+			# stopCluster(cl)
+		}	
 	}
 }
 
--- scripts/assoc.sum.sh
+++ scripts/assoc.sum.sh
@@ -1,43 +1,50 @@
 #!/bin/bash
 
 dir=/work/sph-huangj
-pathX=ppp
-pathY=main
-cis=1 # 通常设为0，当进行PPP的cis-pQTL分析时设为0
+folderX=ppp
+folderY=main
+folderM="mb met" # bb bc mb met ppp  # 改变之前的代码，避免X-Y为每一个folderM重复跑🏃‍
 
-for Y in bald12 bald13 bald14 bald134 bald1234; do
+cis=1 # 通常设为0，当进行PPP的cis-pQTL分析时设为1
+cisMRcML=0
+coloc=0
+mrMed=1 # 通常设为0，会消耗大量计算🏂
+
+for Y in bald12; do # bald13 bald14 bald134 bald1234; do
 	outdir=$dir/analysis/assoc.sum.NEW/$Y; mkdir -p $outdir
 
 	for X in CA5A EPGN PAM PXN FES UXS1; do # ALL
 	echo $X
-	if [ $X == $Y ]; then continue; fi
-
-	for pathM in mb met; do # bb bc mb met ppp 或 NA
-	label="$X-$pathM-$Y"
 	# if [ -f $outdir/$label.log ]; then echo $label already run; continue; fi
+	if [ $X == $Y ]; then continue; fi
+	label="$X-$Y"
 
+	if [ $cis == 1 ]; then 
+		str=`awk -v p=$X '{if ($4==p) print $1, $2, $3}' $dir/files/ppp.glist-hg19`
+		read -r chr pos_begin pos_end <<< $str
+		if [ $chr == "" ]; then
+			cis_str=0 # 没办法，找不到位置
+		else
+			cis_str="1; chr=$chr; pos_begin=$pos_begin; pos_end=$pos_end; flank=1000000"
+		fi
+	else
+		cis_str=0
+	fi
+	
 	echo "#!/bin/bash
 	module load python/anaconda3/2020.7
 	source activate
 	conda activate R
-	cat $dir/scripts/main/assoc.sum.R | sed '9 s/?/$pathX/; 10 s/?/$pathY/; 11 s/?/$pathM/; 13 s/?/$X/; 14 s/?/$Y/; 15 s/?/$label/' > $label.R
+	cat $dir/scripts/main/assoc.sum.R | sed '8 s/?/$folderX/; 9 s/?/$folderY/; 10 s/?/$folderM/; 11 s/?/$X/; 12 s/?/$Y/; 13 s/?/$label/; 15 s/?/$cis_str/; 16 s/?/$cisMRcML/; 17 s/?/$coloc/; 18 s/?/$mrMed/' > $label.R
+	R CMD BATCH $label.R
 	" > $outdir/$label.cmd
-	
-	if [ $cis == 1 ]; then 
-		str=`awk -v p=$X '{if ($4==p) print $1, $2, $3}' $dir/files/ppp.glist-hg19`
-		read -r chr pos_begin pos_end <<< $str
-		if [ $chr == "" ]; then continue; fi
-		echo "sed -i '12 s/cis=0/cis=1; chr=$chr; pos_begin=$pos_begin; pos_end=$pos_end; flank=1000000/' $label.R" >> $outdir/$label.cmd
-	fi
-	echo "R CMD BATCH $label.R" >> $outdir/$label.cmd
 
 	cd $outdir
 	bsub -q short -n 40 -J sum.$label -o $label.LOG -e $label.ERR < $label.cmd
 done
 done
-done
 
-# awk 'FNR==1 && $1 !~ /SKIP/' */*-met-*.log | cut -d ' ' -f 2,4,6-8 | sort -k 5,5g > mr.top.txt
+# awk 'FNR==1 && $1 !~ /SKIP/' */*.mr.log | cut -d ' ' -f 2,4,6-8 | sort -k 5,5g > mr.top.txt
 # echo "X M Y iv X2Y.b X2Y.p TOT.b TOT.p ACME.b ACME.p ADE.b ADE.p Prop.b Prop.p" > mrMed.top.txt; 
-# cat */*.log | cut -d ' ' -f 2-4,6-8,10-17 | awk 'NF==14 && $9*$11 >0 && $8<0.01 && $10<0.01 && $14<0.01 && $13>=0.1' | sed 's/"//g' | grep -v "^id" | sort -k 13,13gr >> mrMed.top.txt
+# cat */*mrMed.log | cut -d ' ' -f 2-4,6-8,10-17 | awk 'NF==14 && $9*$11 >0 && $8<0.01 && $10<0.01 && $14<0.01 && $13>=0.1' | sed 's/"//g' | grep -v "^id" | sort -k 13,13gr >> mrMed.top.txt
 
